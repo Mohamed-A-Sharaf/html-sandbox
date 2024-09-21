56dd43c450fc78a655d14bc197d23cb00d821bba
Mohamed Sharaf <mohamed.a.sharaf.maro@gmail.com>
2024-09-22 00:08:25 +0300

    First Commit

---
+++ 08-various-css-features/04-sticky-navbar/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".header");
+  function toggleHeaderTranspaerncy() {
+    if (window.scrollY > 0) {
+      header.classList.add("transparent");
+    } else {
+      header.classList.remove("transparent");
+    }
+  }
+  window.addEventListener("scroll", toggleHeaderTranspaerncy);
+});
